87929d137efead10aa2c82da936f8bd6786cbfd5
izanbermejo <izanbermejofp@ibf.cat>
2025-12-05 11:52:29 +0100

    Creada la card para mostrar producto

--- view/Carta.php
+++ view/Carta.php
@@ -61,7 +61,30 @@
         <div class="titulo-subcategoria">
           <h2><?= $subcategoria->getNombreSubcategoria() ?></h2>
         </div>
-      </div>
+          <!-- Cards productos -->
+        <div class="cards-productos d-flex flex-row justify-content-center">
+          <div class="card-producto card overflow-hidden">
+            <div class="card-producto-header card-header bg-secondary d-flex align-items-center">
+              <h3>Pizza Margarita</h3>
+            </div>
+              <img class="img-producto" src="public/assets/productos/imagen_producto_pizzas_margarita.png" alt="">
+              <div class="card-ingrediente-body card-body d-flex flex-column justify-content-between">
+                <div class="ingredientes-caracteristicas d-flex flex-row flex-nowrap">
+                  <p class="card-producto-text card-text overflow-hidden">Salsa de tomate, mozzarela, albahaca.</p>
+                  <div class="caracteristicas-producto d-flex flex-row align-items-center">
+                    <img src="public/assets/caracteristicasNutricionales/icono_caracteristica_nutricional_gluten.png" alt="">
+                    <img src="public/assets/caracteristicasNutricionales/icono_caracteristica_nutricional_lacteos.png" alt="">
+                  </div>
+                </div>
+                <div class="d-flex flex-row justify-content-between">
+                  <button class="btn-anadir-carrito btn btn-primary">Añadir al carrito</button>
+                  <span class="precio">12,99</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
       
     <?php } ?>
   </div>
@@ -103,6 +126,7 @@
   border-top: solid 2px var(--bs-secondary);
   padding-left: 184px !important;
   padding-right: 184px !important;
+  padding-bottom: 74px;
 }
 
 .filtros-productos {
@@ -116,6 +140,52 @@
 
 .titulo-subcategoria {
   color: black;
+}
+
+.cards-productos {
+  gap: 16px;
+}
+
+.card-producto {
+  height: 525px !important;
+  width: 300px !important;
+}
+
+.card-producto-header {
+  height: 100px;
+  padding-left: 26px !important;
+  border: 0px !important;
+}
+
+.card-ingrediente-body {
+  height: 160px;
+  box-shadow: 0px -2px 8px 0px rgba(0,0,0,0.1);
+}
+
+.caracteristicas-producto {
+  gap: 5px;
+}
+
+.caracteristicas-producto img {
+  width: 30px;
+  height: 30px;
+}
+
+.card-producto-text {
+  max-height: 50px;
+}
+
+.ingredientes-caracteristicas {
+  gap: 15px;
+}
+
+.btn-anadir-carrito {
+  padding: 10px 15px !important;
+}
+
+.precio {
+  font-size: 26px;
+  font-weight: bold;
 }
 
 </style>
