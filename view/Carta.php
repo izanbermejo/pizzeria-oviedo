5b50ab1305835d8231b404da893c71d0e9f6a403
izanbermejo <izanbermejofp@ibf.cat>
2025-12-09 12:42:30 +0100

    Migas de pan para la carta en caso de estar filtrando por subcategoria

--- view/Carta.php
+++ view/Carta.php
@@ -1,5 +1,24 @@
 <header class="titulo-pagina">
   <h1>Carta</h1>
+  <!-- Si se esta filtrando por subcategoria aparecen migas de pan -->
+  <?php if (isset($_GET['idsubcategoria'])) { ?>
+    <div style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <!-- recorre las categorias para encontrar la que coincide por el id de la URL -->
+        <?php foreach ($listaCategorias as $categoria) { 
+          if ($_GET['idcategoria'] == $categoria->getIdCategoria()) { ?>
+            <li class="breadcrumb-item"><a href="?controller=Carta&action=index&idcategoria=<?= $_GET['idcategoria'] ?>"><?= $categoria->getNombreCategoria() ?></a></li>
+          <?php } 
+        }  
+        // recorre las subcategorias para encontrar la que coincide por el id de la URL 
+        foreach ($listaSubcategorias as $subcategoria) { 
+          if ($_GET['idsubcategoria'] == $subcategoria->getIdSubcategoria()) { ?>
+            <li class="breadcrumb-item active" aria-current="page"><?= $subcategoria->getNombreSubcategoria() ?></li>
+          <?php } 
+        } ?>
+      </ol>
+    </div>
+  <?php } ?>
 </header>
 
 <section class="selector-categoria">
