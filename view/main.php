607cdab1e941a68986537e49ac9f649dfc6fe2aa
izanbermejo <izanbermejofp@ibf.cat>
2025-12-12 09:51:37 +0100

    En la pagina de administrador se utiliza un footer especial

--- view/main.php
+++ view/main.php
@@ -11,9 +11,15 @@
   <title>Document</title>
 </head>
 <body>
-  <?php include_once 'nav.php' ?>
-  <?php include_once $view; ?>
-  <?php include_once 'footer.php' ?>
+  <?php if (isset($_GET['controller']) && $_GET['controller'] == 'Admin') {
+    // include_once 'view/sidebar.php';
+    include_once $view;
+    include_once 'footerAdmin.php';
+  } else {
+    include_once 'nav.php';
+    include_once $view;
+    include_once 'footer.php';
+  }?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 <script>
 document.addEventListener('DOMContentLoaded', function () {
