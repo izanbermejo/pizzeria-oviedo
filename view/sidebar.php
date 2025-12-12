38d9ec4deeb75242d2ba8dec625eac6efa65adcb
izanbermejo <izanbermejofp@ibf.cat>
2025-12-12 12:06:13 +0100

    SIdebar personalizado con logo, nombre de usuario activo y paginas

--- view/sidebar.php
+++ view/sidebar.php
@@ -1,67 +1,56 @@
 <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
-  <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-    <svg class="bi pe-none me-2" width="40" height="32" aria-hidden="true">
-      <use xlink:href="#bootstrap"></use>
-    </svg>
-    <span class="fs-4">Sidebar</span>
+  <a href="?" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+    <img src="public/assets/imagen_LOGO.png" alt="Logo pizzeria Oviedo" width="40" height="40" class="me-2" />
+    <span class="fs-4">Panel admin</span>
   </a>
   <hr />
   <ul class="nav nav-pills flex-column mb-auto">
     <li class="nav-item">
       <a href="#" class="nav-link active" aria-current="page">
-        <svg class="bi pe-none me-2" width="16" height="16" aria-hidden="true">
-          <use xlink:href="#home"></use>
-        </svg>
-        Home
+        <span>Productos</span>
       </a>
     </li>
     <li>
       <a href="#" class="nav-link text-white">
-        <svg class="bi pe-none me-2" width="16" height="16" aria-hidden="true">
-          <use xlink:href="#speedometer2"></use>
-        </svg>
-        Dashboard
+        <span>Ingredientes</span>
       </a>
     </li>
     <li>
       <a href="#" class="nav-link text-white">
-        <svg class="bi pe-none me-2" width="16" height="16" aria-hidden="true">
-          <use xlink:href="#table"></use>
-        </svg>
-        Orders
+        <span>Pedidos</span>
       </a>
     </li>
     <li>
       <a href="#" class="nav-link text-white">
-        <svg class="bi pe-none me-2" width="16" height="16" aria-hidden="true">
-          <use xlink:href="#grid"></use>
-        </svg>
-        Products
+        <span>descuentos</span>
       </a>
     </li>
     <li>
       <a href="#" class="nav-link text-white">
-        <svg class="bi pe-none me-2" width="16" height="16" aria-hidden="true">
-          <use xlink:href="#people-circle"></use>
-        </svg>
-        Customers
+        <span>Categorias</span>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="nav-link text-white">
+        <span>Usuarios</span>
       </a>
     </li>
   </ul>
   <hr />
   <div class="dropdown">
     <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-      <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2" />
-      <strong>mdo</strong>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" style="height: 23px;" class="me-3">
+        <path fill="#ffffff" d="M320 312C386.3 312 440 258.3 440 192C440 125.7 386.3 72 320 72C253.7 72 200 125.7 200 192C200 258.3 253.7 312 320 312zM290.3 368C191.8 368 112 447.8 112 546.3C112 562.7 125.3 576 141.7 576L498.3 576C514.7 576 528 562.7 528 546.3C528 447.8 448.2 368 349.7 368L290.3 368z" />
+      </svg>
+      <strong><?= $_SESSION['usuario']->getNombreUsuario() ?></strong>
     </a>
     <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-      <li><a class="dropdown-item" href="#">New project...</a></li>
       <li><a class="dropdown-item" href="#">Settings</a></li>
       <li><a class="dropdown-item" href="#">Profile</a></li>
       <li>
         <hr class="dropdown-divider" />
       </li>
-      <li><a class="dropdown-item" href="#">Sign out</a></li>
+      <li><a class="dropdown-item" href="?controller=InicioSesion&action=logoutUsuario">Cerrar Sesión</a></li>
     </ul>
   </div>
 </div>
