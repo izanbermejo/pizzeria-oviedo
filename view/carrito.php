b7a980a48e5377776ca0cb80597eee13bcd4711b
izanbermejo <izanbermejofp@ibf.cat>
2025-12-17 12:53:27 +0100

    html y css de productos en el carrito

--- view/carrito.php
+++ view/carrito.php
@@ -2,8 +2,88 @@
   <h1>Carrito</h1>
 </header>
 
-<section class="carrito d-flex flex-row justify-content-between">
-  <div>test carrito</div>
+<section class="carrito d-flex flex-column justify-content-between gap-5">
+  <!-- cuadro donde sale la lista de productos -->
+  <div class="lista-productos shadow">
+    <!-- Producto en el carrito -->
+    <div class="producto d-flex flex-row justify-content-between align-items-center">
+      <div class="img-nombre-producto d-flex flex-row align-items-center gap-4">
+        <img class="img-producto" src="public/assets/productos/imagen_producto_pizzas_margarita.png" alt="imagen pizza margarita">
+        <div>
+          <h2>Pizza Margarita</h2>
+          <p>Salsa de tomate, mozzarela, albahaca.</p>
+        </div>
+      </div>
+      <div class="precio-cantidad-producto d-flex flex-row align-items-center gap-4">
+        <span class="precio-producto">8,50 €</span>
+        <div class="cantidad-producto d-flex flex-row align-items-center justify-content-between">
+          <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" style="height: 24px"><path fill="white" d="M96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320z"/></svg></button>
+          <span>1</span>
+          <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" style="height: 24px"><path fill="white" d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/></svg></button>
+        </div>
+        <svg class="eliminar-producto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path fill="#d62424" d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"/></svg>
+      </div>
+    </div>
+
+    <!-- Separador entre productos -->
+    <hr class="divisor">
+
+    <div class="producto d-flex flex-row justify-content-between align-items-center">
+      <div class="img-nombre-producto d-flex flex-row align-items-center gap-4">
+        <img class="img-producto" src="public/assets/productos/imagen_producto_pizzas_margarita.png" alt="imagen pizza margarita">
+        <div>
+          <h2>Pizza Margarita</h2>
+          <p>Salsa de tomate, mozzarela, albahaca.</p>
+        </div>
+      </div>
+      <div class="precio-cantidad-producto d-flex flex-row align-items-center gap-4">
+        <span class="precio-producto">8,50 €</span>
+        <div class="cantidad-producto d-flex flex-row align-items-center justify-content-between">
+          <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" style="height: 24px"><path fill="white" d="M96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320z"/></svg></button>
+          <span>1</span>
+          <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" style="height: 24px"><path fill="white" d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/></svg></button>
+        </div>
+        <svg class="eliminar-producto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path fill="#d62424" d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"/></svg>
+      </div>
+    </div>
+
+    <hr class="divisor">
+
+    <div class="producto d-flex flex-row justify-content-between align-items-center">
+      <div class="img-nombre-producto d-flex flex-row align-items-center gap-4">
+        <img class="img-producto" src="public/assets/productos/imagen_producto_pizzas_margarita.png" alt="imagen pizza margarita">
+        <div>
+          <h2>Pizza Margarita</h2>
+          <p>Salsa de tomate, mozzarela, albahaca.</p>
+        </div>
+      </div>
+      <div class="precio-cantidad-producto d-flex flex-row align-items-center gap-4">
+        <span class="precio-producto">8,50 €</span>
+        <div class="cantidad-producto d-flex flex-row align-items-center justify-content-between">
+          <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" style="height: 24px"><path fill="white" d="M96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320z"/></svg></button>
+          <span>1</span>
+          <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" style="height: 24px"><path fill="white" d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/></svg></button>
+        </div>
+        <svg class="eliminar-producto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path fill="#d62424" d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"/></svg>
+      </div>
+    </div>
+  </div>
+
+  <!-- seccion debajo de la lista de productos -->
+  <div class="total-carrito d-flex flex-row justify-content-between align-items-center">
+
+    <!-- Cuadro para aplicar codigo de descuento -->
+    <div class="codigo d-flex flex-row justify-content-between align-items-center gap-4 shadow">
+      <input class="input-codigo" type="text" placeholder="Código de descuento">
+      <button class="aplicar-descuento btn btn-secondary">Aplicar descuento</button>
+    </div>
+
+    <!-- cuadro del total y boton de pagar -->
+    <div class="pagar d-flex flex-row justify-content-between align-items-center gap-4 shadow">
+      <h2>Total: 25,50 €</h2>
+      <button class="btn btn-primary">Proceder al pago</button>
+    </div>
+  </div>
 </section>
 
 <style>
@@ -15,6 +95,92 @@
   padding-right: 184px !important;
   padding-bottom: 74px;
   padding-top: 74px;
+  color: black;
+}
+
+.lista-productos {
+  width: 100%;
+  background-color: white;
+  padding: 30px;
+  border-radius: 16px;
+}
+
+.divisor {
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: solid 1px #cececeff;
+  width: 60%;
+  justify-self: center;
+}
+
+.img-nombre-producto, .precio-cantidad-producto {
+  width: 30%;
+  justify-content: space-around;
+}
+
+.img-producto {
+  width: 130px;
+}
+
+.precio-producto {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.cantidad-producto {
+  background-color: var(--bs-secondary);
+  border-radius: 16px;
+  width: 165px;
+  height: 46px;
+  overflow: hidden;
+  border: solid 3px var(--bs-secondary);
+}
+
+.cantidad-producto button {
+  background-color: var(--bs-primary);
+  border: none;
+  cursor: pointer;
+  height: 100%;
+  width: 40px;
+}
+
+.cantidad-producto span {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.eliminar-producto {
+  height: 28px;
+  cursor: pointer;
+}
+
+.total-carrito > div {
+  background-color: white;
+  padding: 18px;
+  border-radius: 16px;
+}
+
+.codigo {
+  width: 40%;
+}
+
+.pagar {
+  width: 35%;
+}
+
+.input-codigo {
+  border: solid 2px var(--bs-secondary);
+  width: 60%;
+  height: -webkit-fill-available;
+  font-size: 18px;
+  border-radius: 16px;
+  text-align: center;
+}
+
+.aplicar-descuento {
+  width: 40%;
+  font-size: 18px;
+  font-weight: bold;
 }
 
 </style>
