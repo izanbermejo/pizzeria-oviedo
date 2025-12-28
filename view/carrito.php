5d54e61da8e919c542b31a703ff53ec1d1612b3f
izanbermejo <izanbermejofp@ibf.cat>
2025-12-28 16:52:10 +0100

    se cambia la cantidad de cada producto en el carrito con los botones de + y -

--- view/carrito.php
+++ view/carrito.php
@@ -53,9 +53,9 @@
         <div class="precio-cantidad-producto d-flex flex-row align-items-center gap-4">
           <span class="precio-producto">${producto.precio_producto} €</span>
           <div class="cantidad-producto d-flex flex-row align-items-center justify-content-between">
-            <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" style="height: 24px"><path fill="white" d="M96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320z"/></svg></button>
-            <span>${producto.cantidad}</span>
-            <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" style="height: 24px"><path fill="white" d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/></svg></button>
+            <button class="disminuir-cantidad"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" style="height: 24px"><path fill="white" d="M96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320z"/></svg></button>
+            <span class="num-cantidad-producto">${producto.cantidad}</span>
+            <button class="aumentar-cantidad"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" style="height: 24px"><path fill="white" d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/></svg></button>
           </div>
           <svg class="eliminar-producto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path fill="#d62424" d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"/></svg>
         </div>
@@ -63,10 +63,42 @@
       <hr class="divisor">
     `;
 
+    const spanCantidad = divProducto.querySelector('.num-cantidad-producto');
+
+    
+    const btnDisminuirCantidad = divProducto.querySelector('.disminuir-cantidad');
+    btnDisminuirCantidad.addEventListener("click", () => {
+      producto.cantidad -= 1;
+      if (producto.cantidad === 1) {
+        btnDisminuirCantidad.disabled = true;
+        btnDisminuirCantidad.classList.add("disabled");
+      }
+      spanCantidad.textContent = producto.cantidad;
+      guardarCarrito(productosCarrito); 
+    });
+    
+    const btnAumentarCantidad = divProducto.querySelector('.aumentar-cantidad');
+    btnAumentarCantidad.addEventListener("click", () => {
+      producto.cantidad += 1;
+      btnDisminuirCantidad.disabled = false;
+      btnDisminuirCantidad.classList.remove("disabled");
+      spanCantidad.textContent = producto.cantidad;
+      guardarCarrito(productosCarrito);
+    });
+
+    if (producto.cantidad === 1) {
+      btnDisminuirCantidad.disabled = true;
+      btnDisminuirCantidad.classList.add("disabled");
+    }
+
     seccionProductos.appendChild(divProducto);
   });
 
   document.getElementsByClassName("divisor")[productosCarrito.length - 1].remove();
+
+  function guardarCarrito(carrito) {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }
 
   if (cantidadProductos > 0) {
     document.getElementById("productosCarrito").classList.remove("visually-hidden");
@@ -176,6 +208,11 @@
   width: 45%;
   font-size: 18px;
   font-weight: bold;
+}
+
+.disabled {
+  cursor: not-allowed;
+  opacity: 0.5;
 }
 
 </style>
